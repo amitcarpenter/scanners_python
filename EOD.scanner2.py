50516706a1fea1fb7c3810f98a3a113bd5bb4110
amitcarpenter200 <amitcarpenter200@gmail.com>
2024-04-01 19:13:26 +0530

    eod2

--- EOD.scanner2.py
+++ EOD.scanner2.py
@@ -4,10 +4,21 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 csv_urls = {
     "Nifty 50": "https://archives.nseindia.com/content/indices/ind_nifty50list.csv",
-    # Add other categories if needed
+    "Nifty Next 50": "https://archives.nseindia.com/content/indices/ind_niftynext50list.csv",
+    "Nifty 100": "https://archives.nseindia.com/content/indices/ind_nifty100list.csv",
+    "Nifty 200": "https://archives.nseindia.com/content/indices/ind_nifty200list.csv",
+    "Nifty 500": "https://archives.nseindia.com/content/indices/ind_nifty500list.csv",
+    "Nifty Smallcap 50": "https://archives.nseindia.com/content/indices/ind_niftysmallcap50list.csv",
+    "Nifty Smallcap 100": "https://archives.nseindia.com/content/indices/ind_niftysmallcap100list.csv",
+    "Nifty Smallcap 250": "https://archives.nseindia.com/content/indices/ind_niftysmallcap250list.csv",
+    "Nifty Midcap 50": "https://archives.nseindia.com/content/indices/ind_niftymidcap50list.csv",
+    "Nifty Midcap 100": "https://archives.nseindia.com/content/indices/ind_niftymidcap100list.csv",
+    "Nifty Midcap 150": "https://archives.nseindia.com/content/indices/ind_niftymidcap150list.csv"
 }
+
 
 def get_symbols_from_csv(csv_url):
     try:
@@ -94,73 +105,6 @@ def fetch_additional_info_from_csv(symbol, csv_file):
     except Exception as e:
         print(f"Error reading CSV file: {e}")
         return {'Company Name': None, 'Industry': None}
-
-
-# @app.route('/get_eod_scanner_02', methods=['GET'])
-# def get_eod_scanner_02():
-#     category = "Nifty 50"
-#     start_date = '2022-03-20'
-#     end_date = '2024-03-28'
-#     csv_file_url = csv_urls.get(category)
-    
-#     if csv_file_url is None:
-#         return jsonify({'message': f"No CSV URL found for category: {category}."})
-
-#     symbols = get_symbols_from_csv(csv_file_url)
-#     if not symbols:
-#         return jsonify({'message': f"No symbols found for category: {category}."})
-
-#     data = pd.DataFrame()
-#     for symbol in symbols:
-#         stock_data = fetch_stock_data(symbol)
-#         additional_info = fetch_additional_info_from_csv(symbol, csv_file_url)
-#         stock_data = yf.download(symbol, start=start_date, end=end_date)
-#         if not stock_data.empty:
-#             ticker = yf.Ticker(symbol)
-#             hist = ticker.history(period="max")
-#             first_appeared_date = hist.index[0].strftime("%d %B %Y")
-#             dividends = ticker.dividends
-#             latest_dividend_date = dividends.idxmax() if not dividends.empty else None
-#             formatted_dividend_date = latest_dividend_date.strftime("%d %B %Y") if latest_dividend_date else None
-            
-#         if not stock_data.empty:
-#             high_52 = stock_data['High'].rolling(window=252, min_periods=1).max().iloc[-1]
-#             l_52 = stock_data['Low'].rolling(window=252, min_periods=1).min().iloc[-1]
-                    
-#         if not stock_data.empty:
-#             data = pd.concat([data, stock_data], axis=0)
-
-            
-#         if not stock_data.empty:
-#             data = pd.concat([data, stock_data], axis=0)
-        
-#     if not data.empty:
-#         scanned_stocks = scan_stocks(data)
-        
-#         if not scanned_stocks.empty:
-#             formatted_data = []
-#             for symbol, row in scanned_stocks.iterrows():
-#                 formatted_row = {
-#                     'Symbol': symbol,
-#                     'Stock Name': additional_info['Company Name'],
-#                     'Sector': additional_info['Industry'],
-#                     'LTP': row['Close'],
-#                     'Volume': row['Volume'],
-#                     '52W High': high_52.tolist(), 
-#                     '52W Low': l_52.tolist(),
-#                     'RSI': row['RSI'],
-#                     'Day Change %': row['Day Change %'],
-#                     'First Appeared on': formatted_dividend_date,
-#                     'Dividend Date': first_appeared_date
-                            
-#                 }
-#                 formatted_data.append(formatted_row)
-#             print(formatted_row)
-#             return jsonify(formatted_data)
-#         else:
-#             return jsonify({'message': "No stocks found."})
-#     else:
-#         return jsonify({'message': "No data available for the selected category."})
 
 
 @app.route('/get_eod_scanner_02', methods=['GET'])
